987696657cf329079dd2b3fc57962381f4e4b127
Hennrick Pelloux <pelloux@insat.insa-toulouse.fr>
2021-04-13 16:12:08 +0200

    valeurs négatives et taille index corrigés

--- PjtKEIL_StepSon/PjtKEIL_StepSon/Src/GestionSon.s
+++ PjtKEIL_StepSon/PjtKEIL_StepSon/Src/GestionSon.s
@@ -6,7 +6,7 @@
 ;Section RAM (read only) :
 	area    mesdata,data,readonly
 	IMPORT Son
-MAX equ 719
+	IMPORT LongueurSon
 
 ;Section RAM (read write):
 	area    maram,data,readwrite
@@ -26,15 +26,18 @@ indexTable dcd 0
 	area    moncode,code,readonly
 ; écrire le code ici		
 
-CallbackSon proc
+CallbackSon proc ; -32768 = 0, 32767 = 719
 	ldr		r0, =indexTable
 	ldr		r1, [r0]
-	ldr		r12, =MAX
+	ldr		r12, =LongueurSon
 	ldr		r12, [r12]
 	cmp		r1, r12
-	bhi		ValeurMax
+	bge		ValeurMax
 	ldr		r2, =Son
 	ldrsh	r3, [r2, r1, lsl #1]
+	add		r3, #32768
+	mov		r12, #91
+	udiv	r3, r12
 	add		r1, #1
 	str		r1, [r0]
 	ldr		r0, =SortieSon
@@ -43,4 +46,4 @@ ValeurMax
 	bx lr
 	endp
 
-	END	
+	END
